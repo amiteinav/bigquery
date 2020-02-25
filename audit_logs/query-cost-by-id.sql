d7e745edcd575227cae6366c03794d864f696195
Amit Einav <amiteinav@google.com>
2020-02-25 18:26:47 +0200

    Create query-cost-by-id.sql

---
+++ audit_logs/query-cost-by-id.sql
@@ -0,0 +1,17 @@
+WITH data as
+  (
+    SELECT
+      protopayload_auditlog.authenticationInfo.principalEmail as principalEmail,
+      protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent AS jobCompletedEvent
+    FROM
+      `MYPROJECTID.MYDATASETID.cloudaudit_googleapis_com_data_access_YYYYMMDD`
+  )
+  SELECT
+    principalEmail,
+    FORMAT('%9.2f',5.0 * (SUM(jobCompletedEvent.job.jobStatistics.totalBilledBytes)/POWER(2, 40))) AS Estimated_USD_Cost
+  FROM
+    data
+  WHERE
+    jobCompletedEvent.eventName = 'query_job_completed'
+  GROUP BY principalEmail
+  ORDER BY Estimated_USD_Cost DESC
